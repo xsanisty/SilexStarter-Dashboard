2abad8e344131bfbe3a5f60879eaf7e96ee27af3
Ikhsan Agustian <ikhsan017@gmail.com>
2015-03-25 10:28:45 +0700

    add menu service provider

--- src/Admin/Resources/config/services.php
+++ src/Admin/Resources/config/services.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
-    'Xsanisty\Admin\Provider\RepositoryServiceProvider'
+    'Xsanisty\Admin\Provider\RepositoryServiceProvider',
+    'Xsanisty\Admin\Provider\SidebarMenuServiceProvider',
 ];
