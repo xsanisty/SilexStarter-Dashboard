d1671066e7a1b692b2a9c112335532f935089978
Ikhsan Agustian <ikhsan017@gmail.com>
2015-06-08 19:26:36 +0700

    add json response for unauthorized ajax request

--- src/Admin/Resources/middlewares.php
+++ src/Admin/Resources/middlewares.php
@@ -1,14 +1,31 @@
 <?php
 
 /** put your module middlewares here */
-App::filter('admin.auth', function(){
-    if(!Sentry::check()){
-        return Response::redirect(Url::to('admin.login'));
+App::filter(
+    'admin.auth',
+    function () {
+        if (!Sentry::check()) {
+            return  Request::ajax()
+            ? Response::json(
+                [
+                    'success'   => false,
+                    'error'     => [
+                        'code'      => 401,
+                        'message'   => 'Unauthorized Access'
+                    ]
+                ],
+                401
+            )
+            : Response::redirect(Url::to('admin.login'));
+        }
     }
-});
+);
 
-App::filter('admin.guest', function(){
-    if(Sentry::check()){
-        return Response::redirect(Url::to('admin.home'));
+App::filter(
+    'admin.guest',
+    function () {
+        if (Sentry::check()) {
+            return Response::redirect(Url::to('admin.home'));
+        }
     }
-});
+);
