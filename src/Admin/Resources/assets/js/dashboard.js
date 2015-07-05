f7c9089909a3bfec91dc7b6f18b4b8ea6223d421
Ikhsan Agustian <ikhsan017@gmail.com>
2015-07-05 11:13:29 +0700

    add modal for about page

--- src/Admin/Resources/assets/js/dashboard.js
+++ src/Admin/Resources/assets/js/dashboard.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
     $('ul.sidebar .active a').after('<div class="pointer"><div class="arrow"></div><div class="arrow_border"></div></div>');
+
+    $('#silexstarter-about').click(function(e){
+        e.preventDefault();
+        $('#silexstarter-about-modal').modal('show');
+    });
 });
